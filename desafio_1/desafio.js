5bfbfe21edf534d2e7d3eb9297fba629e85a537a
gilbertovelazquez <gnilberto_1@hotmail.com>
2022-11-27 16:11:18 -0600

    unificar carpetas de desafio

---
+++ desafio_1/desafio.js
@@ -0,0 +1,58 @@
+class ProductManager {
+
+    constructor() {
+        this.products = [] //generar array de productos vacio
+    }
+
+    getProductId = () => {
+        const amount = this.products.length;
+        const productId = (amount > 0) ? this.products[amount - 1].id + 1 : 1; // generar id que no se repitan
+        return productId;
+    }
+
+    getProducts = () => {
+        return this.products
+    }
+
+    getProductById = (productId) => {
+        const productFound = this.products.find(e => e.id == productId)
+        if (productFound) {
+            console.log("El producto buscado es: ", productFound.title);
+        } else {
+            console.log("producto con id :",productId,"Not found");
+        }
+    }
+
+    addProduct = (title, description, price, thumbnail, code, stock) => {   //pasar los parametros que se van a añadir al producto
+        const product = {
+            id: this.getProductId(),  //tomar el id despues de que se verifico que exista y que no este repetido
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        const duplicatedCode = (element) => element.code == product.code;  // buscar que no se repita el codigo
+        if (!this.products.some(duplicatedCode)) {
+            this.products.push(product)
+        } else {
+            console.log("El codigo",product.code,"esta duplicado, ¡no se agregara a la lsita!")
+        }
+
+    }
+
+}
+
+const product = new ProductManager()
+
+// Para añadir productos introducir, titulo, descripcion, precio, imagen, codigo y catidad de stock
+product.addProduct("motherboard", "chipsets: AMD A32", 1450, "https://freepngimg.com/png/14180-motherboard-free-png-image", 1581, 2)
+product.addProduct("mouse gamer", "Programable con 6 Botones, 6 Retroiluminación RGB", 239, "https://www.amazon.com.mx/Free-Programable-Retroiluminación-Interruptor-Independiente/dp/B098SCLJDJ", 2054, 10)
+product.addProduct("Keyboard", "Mechanical Gaming Keyboard, Ultra Compac", 1600, "https://www.amazon.com.mx/HyperX-Alloy-Origins-Mechanical-Compatible/dp/B08XBQ79MN/ref=sr_1_9?keywords=keyboard&qid=1669066332&qu=eyJxc2MiOiI2Ljc5IiwicXNhIjoiNS45MSIsInFzcCI6IjQuNTcifQ%3D%3D&s=electronics&sprefix=keyb%2Celectronics%2C115&sr=1-9", 8147, 4)
+
+
+product.getProductById(1)  // buscar un producto desde su ID
+
+console.log("Lista de productos", product.getProducts()); //mostrar productos
---
+++ desafio_2_FS/desafio2.js
@@ -0,0 +1,146 @@
+const fs = require("fs");
+
+class ProductManagerFilesystem {
+  // nos pide que la clase reciba la ruta / path del archivo donde guardaremos los productos
+  constructor(path) {
+    // en el constructor podemos inicializar variables de la clase, y darle un valor, cada instancia tendra el suyo
+    this.path = path;
+
+    // Para invocar el método al crear la instancia, podemos llamarlo desde acá
+    this.init();
+  }
+
+  // ver si existe el archivo, de no estar crearlo
+  init() {
+    try {
+      const existFile = fs.existsSync(this.path);
+      if (existFile) return;
+
+      // Se crea vacio si no existe el archivo.
+      fs.writeFileSync(this.path, JSON.stringify([]));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Leer productos
+  async getProducts() {
+    try {
+      // leer el archivo 
+      const response = await fs.promises.readFile(this.path, "utf-8");
+
+      // Se convierte respuesta a JS con JSON.parse
+      return JSON.parse(response);
+
+      //promesa rechazada
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async saveProduct({ title, description, price, thumbnail, code, stock }) {
+    try {
+      // Validamos producto (aunque esto ya es más del desafio anterior)
+      if (!title || !description || !price || !thumbnail || !code || !stock)
+        return { error: "Las variables son obligatorias" };
+
+      const newProduct = { title, description, price, thumbnail, code, stock };
+      // vamos a buscar el array de products, para no hacer el readfile aca podemos reutilizar el metodo getProducts
+      const products = await this.getProducts();
+      
+      //agregar ID verificando que no se repita
+      newProduct.id = !products.length
+        ? 1
+        : products[products.length - 1].id + 1;
+        
+      //evitar productos duplicados por codigo
+      const duplicateCode = products.some(e => e.code == newProduct.code)
+      if(!duplicateCode) {
+        products.push(newProduct);
+      } else {
+        console.log("El codigo",newProduct.code,"esta duplicado, ¡no se agregara a la lsita!")
+     }
+     
+     //Escribir en el archivo FS convirtiendolo a objeto
+      await fs.promises.writeFile(this.path, JSON.stringify(products, null, 3));
+
+      //regresar el nuevo producto a agregar
+      return newProduct;
+
+      //promesa rechazada
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+  //obtener el producto por el ID
+  async getProductsById(productId) {
+    try {
+      const productFound = await this.getProducts();
+      const found = productFound.find(e => e.id == productId)
+      if (found) {
+        console.log("El producto buscado es:",found.title);
+    } else {
+        console.log("producto con id: ",productId,"Not found");
+    }
+    // regresar solo el producto buscado
+    return found;
+
+    //promesa no resuelta
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// Creamos una instancia
+// Y simplemente al hacer el new, se va a ejecutar el metodo init que creara el archivo si no existe
+const electronicProducts = new ProductManagerFilesystem(
+  "./electronic-products.json"
+);
+
+// Metodos asincronos para ejecutar 
+const testClass = async () => {
+
+  // añadir producto
+     const productOne = await electronicProducts.saveProduct({
+      code: 1581,
+      title: "motherboard",
+      description: "chipsets: AMD A32",
+      price: 1450,
+      thumbnail: "https://freepngimg.com/png/14180-motherboard-free-png-image",
+      stock: 2
+     });
+
+     const productTwo = await electronicProducts.saveProduct ({
+      code: 2054,
+      title: "mouse gamer",
+      description: "Programable con 6 Botones",
+      price: 239,
+      thumbnail: "https://www.amazon.com.mx/Free-Programable-Retroiluminación-Interruptor-Independiente/dp/B098SCLJDJ",
+      stock: 10
+     }); 
+
+     const productThree = await electronicProducts.saveProduct ({
+      code: 8147,
+      title: "Keyboard",
+      description: "Mechanical Gaming Keyboard",
+      price: 1600,
+      thumbnail: "https://www.amazon.com.mx/HyperX-Alloy-Origins-Mechanical-Compatible/dp/B08XBQ79MN/ref=sr_1_9?keywords=keyboard&qid=1669066332&qu=eyJxc2MiOiI2Ljc5IiwicXNhIjoiNS45MSIsInFzcCI6IjQuNTcifQ%3D%3D&s=electronics&sprefix=keyb%2Celectronics%2C115&sr=1-9",
+      stock: 4
+     });
+
+
+  // Todos los productos
+  const allProducts = await electronicProducts.getProducts();
+  console.log("Lista de productos", allProducts);
+
+
+  //buscar producto por ID
+  const productById = await electronicProducts.getProductsById(1);  // buscar un producto desde su ID
+  console.log(productById);
+  
+};
+
+testClass();
